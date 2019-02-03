554a94d9f835ee23d9b9b7e5436f3669287fa792
Sujay Kumar <official.sujaykumar@gmail.com>
2019-02-04 00:24:18 +0530

    AAGGHHHHH!!!!! SLIDER IS NOT WORKING!!!

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 $(function(){
-  $("#slider").slider();
+  //change color input
+  $("#paintColor").change(function(){
+      $("#circle").css("background-color", $(this).val());
+  });
+  //change lineWidth using slider
+  $("#slider").slider({
+      min: 3,
+      max: 30,
+      slide: function(event, ui){
+          $("#circle").height(ui.value);
+          $("#circle").width(ui.value);
+          ctx.lineWidth = ui.value;
+      }
+  });
 });
