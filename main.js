8187db86ae8da5f2ad19c12163d05cdb1578bf27
Sujay Kumar <official.sujaykumar@gmail.com>
2019-02-05 14:03:13 +0530

    Slider is finally working!!! Turns out CDNs have to be inserted before.

--- main.js
+++ main.js
@@ -4,13 +4,5 @@ $(function(){
       $("#circle").css("background-color", $(this).val());
   });
   //change lineWidth using slider
-  $("#slider").slider({
-      min: 3,
-      max: 30,
-      slide: function(event, ui){
-          $("#circle").height(ui.value);
-          $("#circle").width(ui.value);
-          ctx.lineWidth = ui.value;
-      }
-  });
+  $("#slider").slider();
 });
