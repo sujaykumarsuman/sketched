0edb76fd94f695bbea36e6373f7ad5315fdfff07
Sujay Kumar <official.sujaykumar@gmail.com>
2019-02-06 10:08:29 +0530

    updated js file

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 $(function(){
-  //change color input
-  $("#paintColor").change(function(){
-      $("#circle").css("background-color", $(this).val());
-  });
-  //change lineWidth using slider
+
+
   $("#slider").slider();
+
 });
