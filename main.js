a0586fcb82ba0f61428980dbd70d85f60c37b8c4
Sujay Kumar <official.sujaykumar@gmail.com>
2019-02-07 16:51:39 +0530

    Size circle fixed

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 $(function(){
 
 
-  $("#slider").slider();
+  $("#slider").slider({
+    min: 3,
+    max: 30,
+    slide: function (event, ui) {
+      $("#circle").height(ui.value);
+      $("#circle").width(ui.value);
+    }
+  });
 
 });
