3de910d88a9f1aee2c33df03239ec66d739580e2
Sujay Kumar <official.sujaykumar@gmail.com>
2019-02-13 00:32:53 +0530

    paint & erase functionality added

--- main.js
+++ main.js
@@ -9,5 +9,60 @@ $(function(){
     }
   });
 
-  
+  var paint = false;
+  var paint_erase = "paint";
+  var canvas = document.getElementById("paint");
+  var ctx = canvas.getContext("2d");
+  var container = $("#paintbox");
+  var mouse = {x: 0, y: 0};
+
+  ctx.lineWidth = 3;
+  ctx.lineJoin = "round";
+  ctx.lineCap = "round";
+
+  container.mousedown(function(e){
+    paint = true;
+    ctx.beginPath();
+
+    mouse.x = e.pageX - this.offsetLeft;
+    mouse.y = e.pageY - this.offsetTop;
+    ctx.moveTo(mouse.x, mouse.y);
+  });
+
+  container.mousemove(function(e) {
+    
+    mouse.x = e.pageX - this.offsetLeft;
+    mouse.y = e.pageY - this.offsetTop;
+
+    if(paint == true){
+      if(paint_erase == "paint"){
+        ctx.strokeStyle = "red";
+      }
+      else{
+        ctx.strokeStyle = "white";
+      }
+
+      ctx.lineTo(mouse.x, mouse.y);
+      ctx.stroke();
+    }
+  });
+
+  container.mouseup(function(){
+    paint = false;
+  });
+  container.mouseleave(function(){
+    paint = false;
+  });
+
+  $("#erase").click(function(){
+    if(paint_erase == "paint"){
+      paint_erase = "erase";
+    }
+    else{
+      paint_erase = "paint";
+    }
+
+    $(this).toggleClass("eraseMode");
+  });
+
 });
